352799af776f824b595e4fe2bdea2f4eff895904
Keith Grant <keithjgrant@redhat.com>
2020-05-03 11:32:42 -0700

    setting up configs, first pages

---
+++ src/pages/drinks/bannon-tantrum.md
@@ -0,0 +1,31 @@
+---
+title: "Bannon Tantrum"
+path: "/drinks/bannon-tantrum"
+date: "2018-01-16T11:54:50-05:00"
+glass: rocks
+booziness: 1
+sweetness: 2
+family: sour
+ingredients: [
+  "2 oz Dry Fly vodka",
+  "½ oz meyer lemon juice",
+  "½ oz simple syrup"
+]
+tags: [
+  "summer",
+  "sidecar-original",
+  "vodka",
+  "lemon",
+  "shaken"
+]
+image:
+  url: "/images/drinks/bannon-tantrum.jpg"
+  alt: "A crystal glass with the cocktail in front of a bottle of Dry Fly vodka and a meyer lemon"
+  align: "40% 45%"
+intro: |
+  “The media should keep its mouth shut and just listen for awhile”&mdash;_Steve Bannon throwing a tantrum on Jan 26, 2017_
+
+  This must be made with [Dry Fly brand vodka](http://www.dryflydistilling.com/dryfly-product/washington-wheat-vodka/); It has an amazing wheat flavor and I created this recipe specifically to let it shine.
+---
+
+Add everything to a shaker with ice. Shake. Strain into a rocks glass over ice.
---
+++ src/templates/drink.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { graphql } from 'gatsby';
+
+// import '../css/blog-post.css'; // make it pretty!
+
+export default function DrinkTemplate({ data }) {
+  const { markdownRemark: post } = data; // data.markdownRemark holds your post data
+  return (
+    <div className="blog-post-container">
+      <Helmet title={`${post.frontmatter.title}`} />
+      <div className="blog-post">
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.frontmatter.intro }} />
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export const pageQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        intro
+      }
+    }
+  }
+`;
