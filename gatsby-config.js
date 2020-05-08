a491e611a78aa91af591719995801f10eb93c062
Keith Grant <keithjgrant@redhat.com>
2020-05-07 19:56:41 -0700

    add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,24 @@ module.exports = {
         plugins: ['gatsby-remark-slug'],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Sidecar',
+        short_name: 'Sidecar',
+        start_url: '/',
+        background_color: '#413d40',
+        theme_color: '#f4b071',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
