ec343b5dfd529afdc188808c172d7ea07a6ca203
Keith Grant <keithjgrant@redhat.com>
2020-05-18 09:16:29 -0700

    fix manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,18 +16,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
@@ -54,17 +42,13 @@ module.exports = {
       options: {
         name: 'Sidecar',
         short_name: 'Sidecar',
+        description: 'A curated collection of cocktails',
         start_url: '/',
         background_color: '#413d40',
         theme_color: '#f4b071',
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
         icon: 'src/images/icon.png',
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
+        crossOrigin: 'use-credentials',
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
